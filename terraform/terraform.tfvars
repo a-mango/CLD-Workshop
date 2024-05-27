b52290831c57eb5b69eb35ceec48dbe4090919c3
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2024-05-27 23:03:13 +0200

    rfr: proxmox provisioning script

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,16 +1,5 @@
 proxmox_api_url = "https://192.168.12.20:8006/api2/json"
-proxmox_user = "workshop@pve"
-proxmox_pool = "workshop"
-proxmox_node = "pve0"
-template_id = "rhel9-cloudinit"
-ssh_user = "cloud-user"
+proxmox_api_token_id = "workshop@pve!terraform_prov"
+proxmox_api_token_secret = "a82f5450-3d6c-4414-a6d2-2487b45e8249"
 ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCsLaVoMFRHvHhkbGPb7Vm6svF29rYLWXU3GYMhU5ssuvl3J+2ixoNXzLrIl6Mn0blYVKKU4byBgVbpPCjws6eEui1xrhc0nK3axXIZIzYMSibUZlXCgJGb/cnPz2HklHQGKo+V63P2UMvp5jddsnYJB+1N/iH3Y00irCJnTyk5nfR/EaOF7j0dsQIbAtZ2tbj9mqkBsGN3+t0ySQ2MTwI7PmZ0KYVCto07zAUQg3fGcoTv0lJlU0BZlWilbUsTQ11FEwaaCJkh1pYA1L1c7JMiqNd2zC0Fzy3ZWqhevXsqkLSyQV+LgMRZv4+C3u7q+BwR6ikOnbx9SYwjGF38QiSoC0QcUQ5ZoCzi08Srxzzkxg2lnT78yISyDWzyu1t68LeELNVWbTmDwtdKgCzGK3n5xFJb6OgpLsr2Jq3f70LN1OzUopW1t8S1WaonrlsjFf0GJiZJzlU8yGLaPcaFnAD/d6foe03r0TGmYDxFwh9RqC0cC7ngDR6XeBXig13d7g5fIqEOlMx3XJk7gyFHbBPekIaMJKeuzOiRfeLCQanbqPJc9EiC7HK41T/HE/zBtKNQLED1AAduzlOlJOBM/F6IN/MmASFuKrdDou0oPPU98wuRIyPypfoxrMF0B3m7UnDMKESe5SR0FZUH24dY5G+Eooni4VuDc0n2p/DBbyNekQ=="
-vm_cores = 2
-vm_memory = 8192
-vm_disk_size = "50G"
-vm_password = "workshop"
-vm_network_bridge = "vnet10"
-master_count = 3
-master_id_base = 8000
-worker_count = 3
-worker_id_base = 8010
+ssh_password = "workshop"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,13 +4,13 @@ variable "proxmox_api_url" {
   default     = "https://proxmox.example.com:8006/api2/json"
 }
 
-variable "proxmox_user" {
-  description = "Proxmox user"
+variable "proxmox_api_token_id" {
+  description = "Proxmox API token ID"
   type        = string
 }
 
-variable "proxmox_password" {
-  description = "Proxmox password"
+variable "proxmox_api_token_secret" {
+  description = "Proxmox API token secret"
   type        = string
   sensitive   = true
 }
@@ -18,6 +18,7 @@ variable "proxmox_password" {
 variable "proxmox_node" {
   description = "Proxmox node"
   type        = string
+  default = "pve0"
 }
 
 variable "proxmox_storage" {
@@ -29,12 +30,13 @@ variable "proxmox_storage" {
 variable "proxmox_pool" {
   description = "Proxmox pool name"
   type        = string
-  default     = "default"
+  default     = "workshop"
 }
 
 variable "template_id" {
   description = "Proxmox template ID"
   type        = string
+  default = "rhel9-cloudinit"
 }
 
 variable "ssh_public_key" {
@@ -48,34 +50,34 @@ variable "ssh_user" {
   default     = "cloud-user"
 }
 
-variable "vm_password" {
+variable "ssh_password" {
   description = "VM password"
   type        = string
   sensitive   = true
 }
 
-variable "vm_memory" {
-  description = "Memory for VMs in MB"
-  type        = number
-  default     = 16384
-}
+# variable "vm_sockets" {
+#   description = "Number of sockets for VMs"
+#   type        = number
+#   default     = 1
+# }
 
-variable "vm_cores" {
-  description = "Number of cores for VMs"
-  type        = number
-  default     = 4
+# variable "vm_vcpus" {
+#   description = "Number of vCPUs for VMs"
+#   type        = number
+#   default     = 4
+# }
+
+variable "vm_cpu_type" {
+  description = "CPU type for VMs"
+  type        = string
+  default     = "host"
 }
 
 variable "vm_sockets" {
   description = "Number of sockets for VMs"
   type        = number
   default     = 1
-}
-
-variable "vm_vcpus" {
-  description = "Number of vCPUs for VMs"
-  type        = number
-  default     = 4
 }
 
 variable "vm_disk_size" {
@@ -87,11 +89,11 @@ variable "vm_disk_size" {
 variable "vm_network_bridge" {
   description = "Network bridge"
   type        = string
-  default     = "vmbr0"
+  default     = "vnet10"
 }
 
-variable "gateway" {
-  description = "Gateway IP"
+variable "vm_network_gateway" {
+  description = "vm_network_gateway IP"
   type        = string
   default     = "10.0.10.1"
 }
@@ -103,20 +105,50 @@ variable "master_count" {
   default     = 3
 }
 
+variable "master_id_base" {
+  description = "Base ID for master nodes"
+  type        = number
+  default     = 8000
+}
+
+variable "master_memory" {
+  description = "Memory for master nodes in MB"
+  type        = number
+  default     = 16384
+}
+
+variable "master_cores" {
+  description = "Number of cores for master nodes"
+  type        = number
+  default     = 4
+}
+
+variable "master_vcpus" {
+  description = "Number of vCPUs for master nodes"
+  type        = number
+  default     = 4
+}
+
 variable "worker_count" {
   description = "Number of worker nodes"
   type        = number
   default     = 3
 }
 
-variable "master_id_base" {
-  description = "Base ID for master nodes"
-  type        = number
-  default     = 100
-}
-
 variable "worker_id_base" {
   description = "Base ID for worker nodes"
   type        = number
-  default     = 110
+  default     = 8010
+}
+
+variable "worker_memory" {
+  description = "Memory for VMs in MB"
+  type        = number
+  default     = 8192
+}
+
+variable "worker_cores" {
+  description = "Number of cores for VMs"
+  type        = number
+  default     = 2
 }
