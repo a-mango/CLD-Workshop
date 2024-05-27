89ec3c84fa557b4ca470553980327f1d4a95da5d
Aubry Mangold <31241846+a-mango@users.noreply.github.com>
2024-05-27 23:03:13 +0200

    rfr: proxmox provisioning script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,11 @@
-resource "proxmox_vm_qemu" "master" {
+# PVE Openshift Cluster
+# This module creates a PVE Openshift cluster with a given number of master and worker nodes
+
+# Master nodes
+resource "proxmox_vm_qemu" "pve-master-nodes" {
   count       = var.master_count
   vmid        = var.master_id_base + count.index
-  name        = "rhel9-master-${count.index + 1}"
+  name        = "master-${count.index + 1}"
   target_node = var.proxmox_node
   pool        = var.proxmox_pool
 
@@ -9,11 +13,63 @@ resource "proxmox_vm_qemu" "master" {
 
   os_type = "cloud-init"
   qemu_os = "l26"
-  cores   = var.vm_cores
+  memory  = var.master_memory
+  cores   = var.master_cores
   sockets = var.vm_sockets
-  vcpus   = var.vm_vcpus
-  memory  = var.vm_memory
-  cpu     = "host"
+  cpu     = var.vm_cpu_type
+  scsihw  = "virtio-scsi-single"
+
+  boot     = "c"
+  bootdisk = "order=scsi0"
+
+  agent = 1
+
+  ssh_user  = var.ssh_user
+  sshkeys   = var.ssh_public_key
+  ipconfig0 = "ip=dhcp"
+
+  disks {
+    ide {
+      ide2 {
+        cloudinit {
+          storage = var.proxmox_storage
+        }
+      }
+    }
+    scsi {
+      scsi0 {
+        disk {
+          storage  = var.proxmox_storage
+          size     = var.vm_disk_size
+          iothread = true
+          discard  = true
+        }
+      }
+    }
+  }
+
+  network {
+    model  = "virtio"
+    bridge = var.vm_network_bridge
+  }
+}
+
+# Worker nodes
+resource "proxmox_vm_qemu" "pve-worker-nodes" {
+  count       = var.worker_count
+  vmid        = var.worker_id_base + count.index
+  name        = "worker-${count.index + 1}"
+  target_node = var.proxmox_node
+  pool        = var.proxmox_pool
+
+  clone = var.template_id
+
+  os_type = "cloud-init"
+  qemu_os = "l26"
+  memory  = var.worker_memory
+  cores   = var.worker_cores
+  sockets = var.vm_sockets
+  cpu     = var.vm_cpu_type
   scsihw  = "virtio-scsi-single"
 
   boot     = "c"
